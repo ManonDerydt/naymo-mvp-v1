523b3a1155523936b22e89a106f318435898cd01
ManonDerydt <72386063+ManonDerydt@users.noreply.github.com>
2025-08-16 10:16:33 +0200

    Fix useState import in Sidebar

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { Link, useLocation } from 'react-router-dom'
 import { Store, LayoutDashboard, Tag, Users, Settings } from 'lucide-react'
 import { cn } from '@/lib/utils'

7ea2b0f6143f30e0559da4477bff54263e8fd4cf
ManonDerydt <72386063+ManonDerydt@users.noreply.github.com>
2025-09-21 22:19:53 +0200

    Updated Layout.tsx

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -4,9 +4,12 @@ import TopBar from './TopBar'
 
 const Layout = () => {
   return (
-    <div className="min-h-screen bg-gray-50">
+    <div className="min-h-screen bg-gray-50 flex">
+      {/* Menu à gauche */}
       <Sidebar />
-      <div className="lg:pl-64 flex flex-col min-h-screen">
+
+      {/* Contenu à droite */}
+      <div className="flex flex-col flex-1 min-h-screen">
         <TopBar />
         <main className="flex-1 p-6">
           <div className="max-w-7xl mx-auto">
